5653801cbcb5e324c1577e4870ac1c60603dec25
Lukas Kollmer <lukas.kollmer@gmail.com>
2017-04-21 22:21:55 +0200

    utils.cc/.h: don't import the entire v8 namespace, this causes problems w/ corefoundation

--- src/binding/utils.cc
+++ src/binding/utils.cc
@@ -4,13 +4,13 @@
 
 #include "utils.h"
 
-const char *ValueToChar(Isolate *isolate, Local<Value> val) {
+const char *ValueToChar(v8::Isolate *isolate, v8::Local<v8::Value> val) {
     if(!val->IsString()){
-        isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate, "Argument Must Be A String")));
+        isolate->ThrowException(v8::Exception::TypeError(v8::String::NewFromUtf8(isolate, "Argument Must Be A String")));
         return NULL;
     }
 
-    String::Utf8Value val_string(val);
+    v8::String::Utf8Value val_string(val);
     char * val_char_ptr = (char *) malloc(val_string.length() + 1);
     strcpy(val_char_ptr, *val_string);
     return val_char_ptr;
--- src/binding/utils.h
+++ src/binding/utils.h
@@ -8,9 +8,8 @@
 #include <node.h>
 #include <v8.h>
 
-using namespace v8;
-
-const char *ValueToChar(Isolate *isolate, Local<Value> val);
+const char *ValueToChar(v8::Isolate *isolate, v8::Local<v8::Value> val);
 
 #define EQUAL(s1, s2) strcmp(s1, s2) == 0
+
 #endif //OBJC_UTILS_H
