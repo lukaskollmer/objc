bf6808bd37c67e42e7853d77eebde319a781de0b
Lukas Kollmer <lukas.kollmer@gmail.com>
2017-05-16 11:31:40 +0200

    Added a new example: NSArray

---
+++ examples/nsarray.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const objc = require('../src/index.js');
+
+const {
+	NSArray
+} = objc;
+
+let array = NSArray.arrayWithArray_(["Hey", "how", "are", "you"]);
+console.log("array:", array);
+
+console.log("count:", array.count());
+
+console.log("array[1]:", array.objectAtIndex_(1));
+console.log("first object:", array.firstObject());
