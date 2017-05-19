483cc0e92b64349d1eb2856455b5705d333a5708
Lukas Kollmer <lukas.kollmer@gmail.com>
2017-05-19 13:05:03 +0200

    updated date example

--- examples/date.js
+++ examples/date.js
@@ -3,15 +3,22 @@
 const objc = require('../src/index.js');
 
 const {
-	NSDate
+	NSDate,
+	NSDateFormatter
 } = objc;
 
 let now = NSDate.date();
 console.log(now);
 
-// Convert the NSDate object to a JavaScript date
+ Convert the NSDate object to a JavaScript date
 let asJSDate = objc.js(now);
 console.log(asJSDate);
 
 let asNSDate = objc.ns(asJSDate);
 console.log(asNSDate);
+
+
+let localizedDate = NSDateFormatter.localizedStringFromDate_dateStyle_timeStyle_(now, 2, 2);
+console.log(String(localizedDate)); // -> "19. Apr 2017, 22:41:13"
+
+
