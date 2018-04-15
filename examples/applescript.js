a4954e9c631c77dc4703514b85ad9cbb6a166de5
Lukas Kollmer <lukas.kollmer@gmail.com>
2018-04-15 19:54:10 +0200

    updated examples/applescript.js

--- examples/applescript.js
+++ examples/applescript.js
@@ -1,15 +1,9 @@
-'use strict';
-
 const objc = require('../src/index');
 
 const {NSAppleScript} = objc;
 
-const source = `tell application "System Events" to get name of current user`;
-
-const script = NSAppleScript.alloc().initWithSource_(source);
-console.log(`Initialized AppleScript object: ${script}`);
+const script = NSAppleScript.alloc().initWithSource_(`tell application "System Events" to get name of current user`);
 
 const err = objc.allocRef();
 const result = script.executeAndReturnError_(err);
-console.log(result, err.description());
-
+console.log(result, err);
