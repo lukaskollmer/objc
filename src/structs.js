fe7a994857db6f70607ac8f3ed089348150e00cb
Lukas Kollmer <lukas.kollmer@gmail.com>
2018-10-15 14:23:28 +0200

    [structs] Define a `StructType.new` initializer

--- src/structs.js
+++ src/structs.js
@@ -2,6 +2,30 @@ const struct = require('ref-struct');
 
 const structs = {};
 
+const createStructInitializer = (name, StructType) => {
+  const fields = Object.getOwnPropertyNames(StructType.fields).sort((key0, key1) => {
+    return StructType.fields[key0].offset - StructType.fields[key1].offset;
+  });
+
+  StructType.new = function (...args) {
+    if (args.length === 0) {
+      return new StructType();
+    }
+
+    if (fields.length !== args.length) {
+      throw new TypeError(`Invalid number of fields passed to '${name}' constructor. Expected ${fields.length}, got ${args.length}`);
+    }
+
+    const value = new StructType();
+    args.forEach((arg, index) => {
+      value[fields[index]] = arg;
+    });
+    return value;
+  };
+
+  return StructType;
+};
+
 module.exports = {
   defineStruct: (name, fields) => {
     if (name in structs) {
@@ -9,7 +33,7 @@ module.exports = {
     }
     const type = struct(fields);
     structs[name] = type;
-    return type;
+    return createStructInitializer(name, type);
   },
 
   getStructType: name => structs[name]
